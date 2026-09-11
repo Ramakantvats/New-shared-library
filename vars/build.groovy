597779da9172f1f90f68cc336067228a99683187
Ramakant <rvvats6366@gmail.com>
2026-09-11 14:02:32 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,5 +1,5 @@
 def call(){
-  echo "Building the django docker image"
+  echo "Building the django docker image chal be"
   // sh "docker build -t django-image:v1 ."
   echo "Image build successfullyy...."
 }
