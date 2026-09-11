a879c8d7fcfcdaad04c5db0020493477f47c2e85
Ramakant <rvvats6366@gmail.com>
2026-09-11 13:07:08 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,5 @@
 dev call(){
   echo "Clonning the Django-code..."
-  git url: "https://github.com/LondheShubham153/django-notes-app.git" , branch: "main"
+  git url: "https://github.com/Ramakantvats/django-notes-app.git" , branch: "main"
   echo "Code clonned successfully..."
 }
