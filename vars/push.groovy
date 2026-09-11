994838b17463326e3974ccd557ddd97378ae5ae5
Ramakant <rvvats6366@gmail.com>
2026-09-11 15:13:47 +0530

    Delete vars/push.groovy

--- vars/push.groovy
+++
@@ -1,8 +0,0 @@
-def call(){
-  echo "Pushing the docker image on docker hub"
-  withCredentials([usernamePassword(credentialsId:"dockerHubCred", passwordVariable:"dockerHubPass", usernameVariable:"dockerHubUser")]){
-  sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
-  sh " docker image tag django-image:v1 ${env.dockerHubUser}/django-image:v1"
-  sh "docker push ${env.dockerHubUser}/django-image:v1 "
-  echo "Image pushed successfully.."
-}
