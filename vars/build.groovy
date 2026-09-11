a515eface69fbf937a1cbbcd0ae6e1a8fca211c2
Ramakant <rvvats6366@gmail.com>
2026-09-11 14:11:46 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,6 +1,5 @@
 def call(){
-  echo "Building the django docker image chal be clonngin the code in build stage"
-  // sh "docker build -t django-image:v1 ."
-  git url: "https://github.com/Ramakantvats/django-notes-app.git", branch: "main"
-  echo "Image build successfullyy.... clonned"
+  echo "Building the django docker image....."
+  sh "docker build -t django-image:v1 ."
+  echo "Image build successfullyy.... "
 }
